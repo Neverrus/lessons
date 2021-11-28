297f2126c2dc10c9346303c3a5de8112a2146742
Neverrus <neverrushs@gmail.com>
2021-11-28 14:56:34 +0300

    add homework solution_02

--- lesson_06/solution_02.py
+++ lesson_06/solution_02.py
@@ -12,23 +12,3 @@ def class_func(class_1, class_2, class_3, desk):
     print(all_desk)
     return
 class_func(class_1=28,class_2=31,class_3=29,desk=2)
-
-
-
-
-
-
-
-
-
-
-"""
-def class_func(class_1, class_2, class_3):
-    cabinet_1 = (class_1 // 2)
-    cabinet_2 = (class_2 // 2)
-    cabinet_3 = (class_3 // 2)
-    all_desk = (cabinet_1 + cabinet_2 + cabinet_3)
-    print(all_desk)
-    return
-class_func(class_1=input("Class 1 size: "),class_2=input("Class 2 size: "),class_3=input("Class 3 size: "))
-"""
